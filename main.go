5f1142c4a97c2fbbbe338961489448adbd96408c
sufuwang <sufuwang@tencent.com>
2021-07-31 09:36:50 +0800

    f: air-HMR

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"tally-go/src"
+)
+
+func main() {
+	fmt.Println("It is Main Function in main.go")
+	src.Test()
+}
---
+++ src/index.go
@@ -0,0 +1,7 @@
+package src
+
+import "fmt"
+
+func Test() {
+	fmt.Println("It is a test Function in drc/index.go")
+}
