7250787032b174189ad3be2fccd5bb10fb5bf8da
sufuwang <sufuwang@tencent.com>
2021-07-31 12:22:13 +0800

    https

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"tally-go/src"
+	"log"
+	"net/http"
 )
 
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("This is an example server.\n"))
+}
+
 func main() {
-	fmt.Println("It is Main Function in main.go")
-	src.Test()
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServeTLS(":1443", "src/auth/cert.pem", "src/auth/key.pem", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
